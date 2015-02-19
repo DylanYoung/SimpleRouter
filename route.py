dd460c5c6fc6a3a5be3c4faa00e31a86b145bf07
Dylan Young <dylanyoungmeijer@gmail.com>
2015-02-19 01:53:40 -0400

    Basic structure complete (untested)

--- route.py
+++ route.py
@@ -1,21 +1,17 @@
-from hashlib import md5
-import mmap
 from socket import (
 			socket, AF_INET,
 			SOCK_STREAM, SOL_SOCKET, 
 			SO_REUSEADDR, gethostname
 			)
-import json
 from threading import Thread, Timer, Lock
 from argparse import ArgumentParser
 import os
-from datetime import datetime
-
 
 ACCESS_ADDR = 'localhost'
 PORT = 23456
 FOUND = "FOUND\0\0\0\0"
 NOTFOUND = "NOTFOUND\0"
+RESERVED = -1
 lock = Lock()
 
 def parseargs():
@@ -45,84 +41,98 @@ def parseargs():
 
 	return pars.parse_args()
 
-def serversocket():
+def serversocket(port):
 	s = socket(AF_INET, SOCK_STREAM)
 	s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-	s.bind((ACCESS_ADDR,PORT))
+	s.bind((ACCESS_ADDR,port))
 	return s
 
-def flush(cache):
-	lock.acquire()
-	with open("cache.json", 'w') as cache_f:
-		json.dump(cache,cache_f)
-	lock.release()
+class ConnectionHandler(Thread):
+	def __init__(self, port = PORT, size = 255):
+		self.server = serversocket(port)
+		self.connections = {}
+		self.free = {i for i in range(size)}
+		self.connections[0] = RESERVED
+		self.free.remove(0)
+		self.__len__ = size
 
+	def __repr__(self):
+		return str(self.connections)
 
-def handlerequest(serv, cache):
-	while 1:
-		sock = serv.accept()[0]
-		request = ""
+	def __str__(self):
+		return str(self.connections)
+
+	def __parse_message__(self, message):
+		mtuple = message.split(":", 2)
+		try: 
+			mtuple[0] = int(mtuple[0])
+			mtuple[1].trim()
+		except ValueError as e:
+			return -1
+		return mtuple
+
+	def __len__(self):
+		return self.__len__
+
+	def add(self, client):
+		try:
+			n = self.free.pop()
+		except KeyError:
+			print "Error: connection rejected"
+			return
+		client.send("n\r\n")
+		self.connections[n] = client
+		t = Thread(target=route, args=(self, client,n,))
+		t.start()
+
+	def remove(self,key):
+		self.free.add(key);
+		self.connections.pop(key, ()).close()
+
+	def route(self, client, n):
 		while 1:
-			print "..."
-			part = sock.recv(80000)
-			if len(part) < 0:
-				break;
-			else: request += part;
-		print request
-		"\r\n".join(request.split('\n', 2)[0:1])
-		key = md5.update(request).digest()
-		responses = cache.get(key, [])
-		if not responses == []:
-			sock.send(FOUND)
-			for response in responses:
-				sock.send(response)
-		else:
-			sock.send(NOTFOUND)
+			try:
+				msg = client.recv(80)
+				mtuple = __parse_message__(message)
+				if mtuple == -1:
+					client.send("Error: invalid message \"" + message + "\"")
+				elif mtuple[0] < 1 or > len(self):
+					client.send("Error: " + mtuple[0] + " not found"
+				elif mtuple[0] == len(self):
+					self.broadcast(message)
+				else:
+					if mtuple[1][0] == '0' and len(mtuple[1]) == 1:
+						self.remove(mtuple[0]) 
+					else: 
+						msg = str(n) + ": " + mtuple[1]
+						self.connections[mtuple[0]].send(msg)
+			except:
+				break
+		self.remove(n)
 
-			responses = []
-			while 1:
-				response = recv(80000)
-				print response
-				responses.append(response)
-				if len(response) < 1:
-					break;
-			lock.acquire()
-			cache[key] = responses
-			lock.release()
-		sock.close()
+	def broadcast(self, msg):
+		for key, connection in self.connections.iteritems():
+			connection.send(msg)
+
+	def run(self):
+		while 1:
+			client = self.server.accept()
+			self.add(client)
+		self.close()
+
+	def close(self):
+		for key in self.connections:
+			self.remove(key)
+		self.server.close()
+		del(self)
+
 
 def main():
 
-	args = parseargs()
-	threads = [-1]*5
-	print len(threads)
-	print NOTFOUND
-	print FOUND
-	# Load existing cache
-	try: 
-		with open("cache.json",'r') as cache_f:
-			cache = json.load(cache_f)
-	except IOError:
-		os.rename("cache.json",
-			datetime.now().time().isoformat()+"cache.json")
-		cache = dict()
-		with open("cache.json", 'w') as cache_f:
-			json.dump(cache,cache_f)
-		
+	parse_args()
+	handle = ConnectionHandler()
+	handle.start()
+	handle.join()
 
-	serv = serversocket()
-	serv.listen(5)
-	Timer(30, flush,(cache,)).start()
-	for t in threads:
-		t = Thread(target=handlerequest, args=(serv,cache,))
-		threads.append(t)
-		t.start()
-
-		# buf = mmap.mmap(-1,mmap.PAGESIZE*20, 
-		#					mmap.MAP_SHARED, 
-		#					mmap.PROT_READ)
-	for t in threads:
-		t.join()
-	serv.close()
 if __name__ == '__main__':  
 	main()
